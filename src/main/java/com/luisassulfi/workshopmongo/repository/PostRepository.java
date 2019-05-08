7eb2ad797b7522830371d790c6919ad2d22d45d1
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-08 13:21:14 -0300

    Consulta simples com @Query

--- src/main/java/com/luisassulfi/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luisassulfi/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,15 @@ package com.luisassulfi.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.luisassulfi.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{'title':{$regex: ?0, $options: 'i'}}")
+	List<Post> findByTitle(String text);
 
 }
