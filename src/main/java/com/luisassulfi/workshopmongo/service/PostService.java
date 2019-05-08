1bc3912df3439d69f9dd624297e9b2e3a120a375
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-07 22:19:48 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luisassulfi/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.luisassulfi.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luisassulfi.workshopmongo.domain.Post;
+import com.luisassulfi.workshopmongo.repository.PostRepository;
+import com.luisassulfi.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
